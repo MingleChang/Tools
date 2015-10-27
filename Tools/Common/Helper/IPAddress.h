f01f8f58633cae9bb237aff36815ad3bb2806a7a
MingleChang <mingle890805@gmail.com>
2015-10-27 09:44:07 +0800

    not

---
+++ Tools/Common/Helper/IPAddress.h
@@ -0,0 +1,13 @@
+//
+//  IPAddress.h
+//  www
+//
+//  Created by cjw on 15/10/27.
+//  Copyright © 2015年 MingleChang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IPAddress : NSObject
++(NSString *)getIPAddress;
+@end
